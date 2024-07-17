4774503084a331064d9dcf329ebb3083edd00785
apollomere <152450012+apollomere@users.noreply.github.com>
2024-07-17 12:58:12 -0700

    Add files via upload

---
+++ .hFiles/card.h
@@ -0,0 +1,16 @@
+#ifndef CARD_H
+#define CARD_H
+
+struct card {
+
+
+
+};
+
+typedef struct card deck;
+
+extern void displayCardType(deck deckData);
+
+#endif
+//Andre: This is just a start, you can change this if needed, this is jsut a vague start for the card deck implementation
+//Andre: I assume we're using character arrays, but I'm not the one responsible so could be different, but I would like to understand.
