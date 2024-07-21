0c434cd469b2e071468b5a8a0898489815ee3402
Jestyn Lytle <122648565+ArgentJava@users.noreply.github.com>
2024-07-21 06:11:30 +0000

    Not done with the commit just yet, so there will be another soon. Made some changed to cards.h which can be seen in my comments. - Jestyn

--- .hFiles/card.h
+++ .hFiles/card.h
@@ -1,15 +1,25 @@
 #ifndef CARD_H
 #define CARD_H
 
-struct card {
+#include <stdio.h>
+#include <stdbool.h>
 
+#define STRLEN 100
 
-
+struct card{
+    char suit[STRLEN+1];
+    char name[STRLEN+1];
+    int pointValue;
+    bool cardIsDrawn;
+    
 };
 
-typedef struct card deck;
+//typedef struct cardDeck deck;
+/*Jestyn: Keeping this a comment for now because we want cards and 
+the deck to be seperate objects. Deck should be an array that stores
+instances of cards. Will revisit this later tho.*/
 
-extern void displayCardType(deck deckData);
+extern void displayCardType(card cardData); //Changed displayCardType to displayCard, updated
 
 #endif
 //Andre: This is just a start, you can change this if needed, this is jsut a vague start for the card deck implementation
