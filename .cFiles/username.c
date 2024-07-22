b7d64837412e93bf1277b9dd7ea88857f2862b6a
apollomere <152450012+apollomere@users.noreply.github.com>
2024-07-22 04:05:39 -0700

    Add files via upload
    Might need to touch up on one thing:
    Data type selection: If it's void no value is returned, however, I don't yet have any reason to see if return value implementation is necessary for the username yet.

---
+++ .cFiles/username.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "username.h"
+#include<string.h>
+void register_name(pn pnData){
+        printf("\n%s is the chosen name",pnData.name);// Output users chosen name.
+        printf("\nPress ENTER to continue, or BACKSPACE to go back.");
+                                        /*Extra option if user is satisfied with name or not. 
+                                                if No: restart the function, 
+                                                if Yes, ensure name is solidified, and end function
+                                                Will probably try to incorporate true/false values, so going to have to look into that-Andre*/
+}
