e1e2f7047ce61eedd71b1a311e6ebb88ae8d51fd
apollomere <152450012+apollomere@users.noreply.github.com>
2024-07-22 12:11:46 -0700

    Update compare.c(Not done yet)

--- .cFiles/compare.c
+++ .cFiles/compare.c
@@ -7,3 +7,16 @@ void displayScore(score scoreData){
     printf("%s Scored:%d, Dealer Score: %d",playername.name, ps, ds);//To include the player-name it must become global, also loop maybe be incorporated in the main for the sake of the struct values.
                                                                     //Definitely not done yet, gonna have to figure out the loop in a seperate file, will port to main if I have something substantial, but, probably wont be done during or after class(got to figure out memory location tricks, bend the rules)
 }
+
+/*
+//Driver Code
+int main () {
+
+
+//Compare Function
+  for(int i = 0; i < sz; i++);//Size needs to be dynamic for DECK type objects(malloc utility?) wondering how to effectively implement it at the moment.
+  
+  return 0;
+};
+
+*/
