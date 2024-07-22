584f07106b4db53e1a5a6162392600ab7008b2cc
apollomere <152450012+apollomere@users.noreply.github.com>
2024-07-22 11:54:14 -0700

    Update compare.c(Note done yet)

--- .cFiles/compare.c
+++ .cFiles/compare.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "compare.h"
-
+#include "username.h"
 
 
 void displayScore(score scoreData){
-    
+    printf("%s Scored:%d, Dealer Score: %d",playername.name, ps, ds);//To include the player-name it must become global, also loop maybe be incorporated in the main for the sake of the struct values.
+                                                                    //Definitely not done yet, gonna have to figure out the loop in a seperate file, will port to main if I have something substantial, but, probably wont be done during or after class(got to figure out memory location tricks, bend the rules)
 }
