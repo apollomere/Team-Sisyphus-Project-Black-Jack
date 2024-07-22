71053ade4a1d51e512c0a85f1104d27260dc7d6e
apollomere <152450012+apollomere@users.noreply.github.com>
2024-07-22 10:55:02 -0700

    Update jestyn_andre_main_c_file_Blac_Jac.c

--- jestyn_andre_main_c_file_Blac_Jac.c
+++ jestyn_andre_main_c_file_Blac_Jac.c
@@ -3,11 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdiolib.h>
-
+#include "username.h"
 //Driver Code
 int main () {
 
-
+//Player Name Section Start//
+  pn playername;//Struct Variable inititalization(pn for playername)
+  printf("Enter your name: ");
+  scanf("\n%s", playername.name);/*Note: Typically & required for scanf, but, got errors for it, could just be me, 
+  worked on gdbonline, but got "collect2.exe: error: ld returned 1 exit status" Not sure how to fix it yet. x_x-Andre*/
+  
+  register_name(playername);//User Input Defined Field, playername assignment.
+//Player Name Section End//
 
   
   
