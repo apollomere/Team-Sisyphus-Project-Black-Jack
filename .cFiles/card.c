badc439f72390d587c7b86d039eba4cf53bce5b1
Jestyn Lytle <122648565+ArgentJava@users.noreply.github.com>
2024-08-07 10:05:00 -0700

    Update card.c
    Added Instances of Ace, 2, 3, and 4 cards. Saving just in case something happens

--- .cFiles/card.c
+++ .cFiles/card.c
@@ -1,6 +1,87 @@
 #include <stdio.h>
 #include "card.h"
+//Including code for reference
+/*struct card{
+    char name[STRLEN+1];
+    int pointValue;
+    bool cardIsDrawn;
+    
+};*/
 
-void displayCard(card cardData);{
+//Instances of Ace cards
+dk dk1;
+dk1.name = "Ace,hearts"
+dk1.pointValue = 1;
+dk1.cardIsDrawn = false;
+dk dk2;
+dk1.name = "Ace,clubs"
+dk1.pointValue = 1;
+dk1.cardIsDrawn = false;
+dk dk3;
+dk1.name = "Ace,spades"
+dk1.pointValue = 1;
+dk1.cardIsDrawn = false;
+dk dk4;
+dk1.name = "Ace,diamonds"
+dk1.pointValue = 1;
+dk1.cardIsDrawn = false;
+
+//Instances of "2" cards
+dk dk5;
+dk1.name = "2,hearts"
+dk1.pointValue = 2;
+dk1.cardIsDrawn = false;
+dk dk6;
+dk1.name = "2,clubs"
+dk1.pointValue = 2;
+dk1.cardIsDrawn = false;
+dk dk7;
+dk1.name = "2,spades"
+dk1.pointValue = 2;
+dk1.cardIsDrawn = false;
+dk dk8;
+dk1.name = "2,diamonds"
+dk1.pointValue = 2;
+dk1.cardIsDrawn = false;
+
+//Instances of "3" cards
+dk dk9;
+dk1.name = "3,hearts"
+dk1.pointValue = 3;
+dk1.cardIsDrawn = false;
+dk dk10;
+dk1.name = "3,clubs"
+dk1.pointValue = 3;
+dk1.cardIsDrawn = false;
+dk dk11;
+dk1.name = "3,spades"
+dk1.pointValue = 3;
+dk1.cardIsDrawn = false;
+dk dk12;
+dk1.name = "3,diamonds"
+dk1.pointValue = 3;
+dk1.cardIsDrawn = false;
+
+//Instances of "4" cards
+dk dk13;
+dk1.name = "4,hearts"
+dk1.pointValue = 4;
+dk1.cardIsDrawn = false;
+dk dk14;
+dk1.name = "4,clubs"
+dk1.pointValue = 4;
+dk1.cardIsDrawn = false;
+dk dk15;
+dk1.name = "4,spades"
+dk1.pointValue = 4;
+dk1.cardIsDrawn = false;
+dk dk16;
+dk1.name = "4,diamonds"
+dk1.pointValue = 4;
+dk1.cardIsDrawn = false;
+
+//Instances of "5" cards
+
+void displayCard(dk* dkData){
 
 }
